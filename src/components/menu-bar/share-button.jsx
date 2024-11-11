17eb8300639997812636b7e05977cd01b81a9fca
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-11-10 18:09:09 -0700

    Update share-button.jsx

--- src/components/menu-bar/share-button.jsx
+++ src/components/menu-bar/share-button.jsx
@@ -28,7 +28,7 @@ const getProjectUri = () => new Promise(resolve => {
 const isUploadAvailable = async () => {
     let res = null;
     try {
-        res = await fetch('https://projects.penguinmod.com/api');
+        res = await fetch('https://projects.penguinmod.com/api/v1');
     } catch {
         // failed to fetch entirely
         return false;
